a476cfdf292e5471a13a2c0f65b5b64b4a3f661c
Micky Kyei <micky.kyei@tuni.fi>
2022-09-22 19:22:15 +0300

    #73 Added routes to app

--- BACKEND/index.ts
+++ BACKEND/index.ts
@@ -1,10 +1,13 @@
 import express, { Application, Request, Response } from "express";
-
+import {CommonRoutesConfig} from './utils/CommonRoutesConfig';
 import bodyParser from "body-parser";
 import cors from "cors";
+import { UserRoutes } from "./routes/user.routes";
+import {TemplateRoutes} from './routes/template.routes';
 
 const app: Application = express();
 const port = 3000;
+const routes: Array<CommonRoutesConfig> = [];
 
 // Use CORS middleware 
 app.use(cors());
@@ -13,6 +16,8 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+routes.push(new UserRoutes(app));
+routes.push(new TemplateRoutes(app));
 
 app.get("/",
     async (req: Request, res: Response): Promise<Response> => {
@@ -22,4 +27,9 @@ app.get("/",
     }
 );
 
-app.listen(port, () => console.log(`OneAWStoGo app listening on port ${port}!`))
+app.listen(port, () => {
+    routes.forEach((route: CommonRoutesConfig) => {
+        console.log(`Routes configured for ${route.getName()}`);
+    });
+    console.log(`OneAWStoGo app listening on port ${port}!`)
+})
