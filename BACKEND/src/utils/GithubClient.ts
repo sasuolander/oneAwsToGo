3e65f2b8bdeae13a3608f275f78405b39e69911e
Sasu <sasuolander@yahoo.com>
2022-09-27 21:50:22 +0300

    #75 change location of comment

--- BACKEND/src/utils/GithubClient.ts
+++ BACKEND/src/utils/GithubClient.ts
@@ -7,9 +7,9 @@ export default class GithubClient {
          return r.data
      })
     }
+    // TODO remove this hard coded value
     async getTemplate(templateName: string,templateURL:string="https://raw.githubusercontent.com/sasuolander/templatesAWS/master/S3_Website_Bucket_With_Retain_On_Delete.yaml"):Promise<string> {
         const dirr = process.env.TEMP_FOLDER + "\\" + templateName
-        // TODO remove this hard coded value
         const response = await this.fetchTemplate(templateURL)
         // TODO check if we need this file creation when deploying template
         Utils.createFile(dirr, response)
