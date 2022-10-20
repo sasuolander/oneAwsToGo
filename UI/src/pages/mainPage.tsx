1ed8949f4aabaa1ce2c6c6bd9cb8e1ea056ce0da
mikko-hakkinen <mikko.hakkinen@tuni.fi>
2022-10-20 18:49:44 +0300

    Added a progress bar for displaying deployment status. Initially hidden. No functionality yet.

--- UI/src/pages/mainPage.tsx
+++ UI/src/pages/mainPage.tsx
@@ -1,12 +1,14 @@
 import "../styles/mainPage.css"
 import CreateEnvironment from "../components/createEnvironment";
 import InfoCard from "../components/infoCard";
+import ProgressBar from "../components/progressBar"
 
 export default function MainPage() {
     return (
             <div id="main-page" className="mainPage">
                 <CreateEnvironment/>
                 <InfoCard/>
+                <ProgressBar/>
             </div>
     );
 }
