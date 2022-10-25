5bd198c334f948c58a9ac33f53db907b9e7af7a8
Sasu <sasuolander@yahoo.com>
2022-10-25 18:52:13 +0300

    remove personal account info

--- infra/runtimeEnviroment/command.ps1
+++ infra/runtimeEnviroment/command.ps1
@@ -3,6 +3,6 @@ npm run build
 cdk deploy
 cdk bootstrap aws://ACCOUNT/eu-west-1 --profile profile
 
-cdk destroy RuntimeEnviromentStack --profile sasuAdmin --require-approval never
-cdk deploy RuntimeEnviromentStack --profile sasuAdmin --require-approval never
- cdk synth | Out-File -FilePath ./templateCloudformation/template.yaml
+cdk destroy RuntimeEnviromentStack --profile <profiles> --require-approval never
+cdk deploy RuntimeEnviromentStack --profile <profiles> --require-approval never
+cdk synth | Out-File -FilePath ./templateCloudformation/template.yaml
