7d9c6572292ba72947562777362c622d8a2ae626
mikko-hakkinen <mikko.hakkinen@tuni.fi>
2022-11-04 18:23:58 +0200

    Deployment is now visualized.

--- UI/src/components/createEnvironment.tsx
+++ UI/src/components/createEnvironment.tsx
@@ -45,36 +45,48 @@ function CreateEnvironment() {
             const name = data[0].field_value
             const cropData = data.filter((f: { field_id: string; }) => f.field_id !== "deployment_name")
             const startProcess = await deployService.triggerCreation(Number(id), name, cropData)
-            setCardVisibility(true);
-            updateCardText("Creating environment with stackId: " + String(startProcess.deploymentId));
-            const internalId = await fetchDeployedService.getDeploymentIdByStackId(String(startProcess.deploymentId));
-            if (internalId !== undefined) {
-                pollStatus(Number(internalId));
-            }
 
+            if (startProcess.httpStatus === 200) {
+                handleDeploymentUpdates(String(startProcess.deploymentId));
+            } else {
+                //TDB in another branch.
+            }
         }
     }
 
-    const pollStatus = async (internalId: Number | undefined) => {
-
+    const handleDeploymentUpdates = async (stackId: string) => {
         const timeout = async (ms:number) => {
             return new Promise(resolve => setTimeout(resolve, ms));
         }
 
-        while (true) {
-            let statusResponse = await deploymentStatusService.fetchDeploymentStatus(Number(internalId));
-            console.log(statusResponse.status);
-            if (statusResponse.status === "CREATE_COMPLETE" || statusResponse.status === "CREATE_FAILED") {
-                break;
+        setCardVisibility(true);
+        setProgressVisibility(true);
+        const internalId = await fetchDeployedService.getDeploymentIdByStackId(stackId);
+        updateCardText("Creating environment with id " + internalId + " and stackId " + stackId);
+
+        if (internalId !== undefined) {
+
+            while (true) {
+                let statusResponse = await deploymentStatusService.fetchDeploymentStatus(Number(internalId));
+                if (statusResponse.status === "CREATE_COMPLETE") {
+                    setProgressVisibility(false);
+                    updateCardText("Successfully created an environment with id " + internalId + " and stackId" + stackId);
+                    break;
+                } else if (statusResponse.status === "CREATE_FAILED") {
+                    setProgressVisibility(false);
+                    updateCardText("Failed to create an environment with id " + internalId + " and stackId " + stackId)
+                    break;
+                } 
+                await timeout(5000);
             }
-            await timeout(5000);
         }
-        
     }
+
 
     const updateCardText = (text: string) => {
         setCardText(text);
     }
+
     useEffect(() => {
         if (!templatesReady) {
             templateService.getTemplates().then(function (response) {
