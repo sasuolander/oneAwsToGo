bc0ba7811e31cd3328b241735a87eb07590d3bb8
Micky Kyei <micky.kyei@gmail.com>
2022-11-20 16:39:09 +0200

    modify seed file to not include templateSourceCode

--- BACKEND/src/database/seeds/templates.ts
+++ BACKEND/src/database/seeds/templates.ts
@@ -8,14 +8,14 @@ const templates = [
         id : 1,
         name: "Website in S3 bucket",
         url:"https://raw.githubusercontent.com/sasuolander/templatesAWS/master/S3_Website_Bucket_With_Retain_On_Delete.yaml",
-        templateFormat: TemplateFormat.CloudFormation, templateSourceCode:"",
+        templateFormat: TemplateFormat.CloudFormation,
         formConfig: JSON.stringify(emptysite)
     },
     {
         id : 2,
         name: "Wordpress site",
         url:"https://raw.githubusercontent.com/sasuolander/templatesAWS/master/WordPress_Single_InstanceOwn.yaml",
-        templateFormat: TemplateFormat.CloudFormation, templateSourceCode:"",
+        templateFormat: TemplateFormat.CloudFormation, 
         formConfig:JSON.stringify(wordpresssite)
     },
   ];
