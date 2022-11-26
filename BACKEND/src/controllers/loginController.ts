8722af3f57fe62c6407305d9847a3ee9931a6bff
Micky Kyei <micky.kyei@gmail.com>
2022-11-26 18:24:28 +0200

    Added some logging

--- BACKEND/src/controllers/loginController.ts
+++ BACKEND/src/controllers/loginController.ts
@@ -1,6 +1,7 @@
 import {CommonControllerConfig} from '../utils/CommonRoutesConfig';
 import {Application, Request, Response} from 'express';
 import UserService from '../services/userService';
+import { logger } from '../utils/logger';
 
 interface ILoginPayload {
     username:string
@@ -22,6 +23,7 @@ export default class LoginController extends CommonControllerConfig {
             const logInfo = req.body as ILoginPayload;
             const logUserIn = await this.userService.checkCredentials(logInfo.username,logInfo.password);
             if(logUserIn) {
+                logger.info("Logged user " + logInfo.username + " in");
                 res.status(200).send();
             } else {
                 res.status(404).send("User with this username/password combination doesn't exist");
